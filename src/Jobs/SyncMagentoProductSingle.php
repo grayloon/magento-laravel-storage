6c1e2179b621828cce4268d4c398d2c13986de71
ahinkle <ahinkle10@gmail.com>
2020-10-13 07:21:06 -0500

    fix: throw error on failed api call

--- src/Jobs/SyncMagentoProductSingle.php
+++ src/Jobs/SyncMagentoProductSingle.php
@@ -2,6 +2,7 @@
 
 namespace Grayloon\MagentoStorage\Jobs;
 
+use Exception;
 use Grayloon\Magento\Magento;
 use Grayloon\MagentoStorage\Events\MagentoProductSynced;
 use Grayloon\MagentoStorage\Support\MagentoProducts;
@@ -40,10 +41,13 @@ class SyncMagentoProductSingle implements ShouldQueue
     public function handle()
     {
         $apiProduct = (new Magento())->api('products')
-            ->show($this->sku)
-            ->json();
+            ->show($this->sku);
 
-        $product = (new MagentoProducts())->updateOrCreateProduct($apiProduct);
+        if (! $apiProduct->ok()) {
+            throw new Exception('Error fetching SKU: '. $this->sku .' Error: '. $apiProduct->json()['body'] ?? 'N/A');
+        }
+
+        $product = (new MagentoProducts())->updateOrCreateProduct($apiProduct->json());
 
         event(new MagentoProductSynced($product));
     }
