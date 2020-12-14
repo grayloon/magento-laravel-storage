6d4e1b067c599bbf26f3d405010f8587407aa722
ahinkle <ahinkle10@gmail.com>
2020-12-14 09:43:37 -0600

    fix: class names

--- src/Database/Migrations/2020_12_14_192428_add_synced_at_to_magento_custom_attribute_types_table.php
+++ src/Database/Migrations/2020_12_14_192428_add_synced_at_to_magento_custom_attribute_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddIsQueuedToMagentoCustomAttributeTypesTable extends Migration
+class AddSyncedAtToMagentoCustomAttributeTypesTable extends Migration
 {
     /**
      * Run the migrations.
