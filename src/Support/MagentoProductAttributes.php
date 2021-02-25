12bb636deccb5708b488dc730fefb93820799813
ahinkle <ahinkle10@gmail.com>
2021-02-25 09:42:03 -0600

    feat: :sparkles: Ability to update or create Product Attributes

--- src/Support/MagentoProductAttributes.php
+++ src/Support/MagentoProductAttributes.php
@@ -4,6 +4,7 @@ namespace Grayloon\MagentoStorage\Support;
 
 use Exception;
 use Grayloon\Magento\Magento;
+use Grayloon\MagentoStorage\Models\MagentoProductAttribute;
 
 class MagentoProductAttributes extends PaginatableMagentoService
 {
@@ -23,5 +24,56 @@ class MagentoProductAttributes extends PaginatableMagentoService
         }
 
         return $attributes->json()['total_count'];
+    }
+
+    /**
+     * Update or create the Magento Product Attribute attribute.
+     *
+     * @param  array  $apiAttribute
+     * @throws \Exception
+     * @return \Grayloon\MagentoStorage\Models\MagentoExtensionAttribute
+     */
+    public function updateOrCreate($apiAttribute)
+    {
+        if (! $apiAttribute['attribute_id']) {
+            throw new Exception('Magento Product Attribute missing Attribute ID: '. json_encode($apiAttribute));
+        }
+
+        $attribute = MagentoProductAttribute::updateOrCreate(['id' => $apiAttribute['attribute_id']], [
+            'name'          => $this->resolveAttributeLabel($apiAttribute['frontend_labels'], $apiAttribute['default_frontend_label']),
+            'code'          => $apiAttribute['attribute_code'],
+            'position'      => $apiAttribute['position'] ?? 0,
+            'default_value' => $apiAttribute['default_value'],
+            'type'          => $apiAttribute['frontend_input'],
+            'synced_at'     => now(),
+        ]);
+        
+        // $this->syncMagentoAttributeOptions($attribute, $apiAttribute['options]);
+
+        return $attribute;
+    }
+
+    /**
+     * Resolve the Product Attribute label by the associated assigned website.
+     *
+     * @param  array   $availableLabels
+     * @param  string  $defaultLabel
+     * @return string
+     */
+    public function resolveAttributeLabel($availableLabels, $defaultLabel)
+    {
+        if (! $availableLabels) {
+            return $defaultLabel;
+        }
+
+        $labels = collect($availableLabels)
+            ->when(config('magento.default_store_id'), function ($collection) {
+                return $collection->filter(fn ($label) => $label['store_id'] == config('magento.default_store_id'));
+            })
+            ->when(! config('magento.default_store_id') && $defaultLabel, function ($collection) use ($defaultLabel) {
+                return $collection->filter(fn ($label) => $label['label'] === $defaultLabel);
+            });
+
+        return $labels->first()['label'];
     }
 }
--- tests/Support/MagentoProductAttributesTest.php
+++ tests/Support/MagentoProductAttributesTest.php
@@ -1,9 +1,11 @@
 <?php
 
 use Exception;
-use Illuminate\Support\Facades\Http;
-use Grayloon\MagentoStorage\Tests\TestCase;
+use Grayloon\MagentoStorage\Database\Factories\MagentoProductAttributeFactory;
+use Grayloon\MagentoStorage\Models\MagentoProductAttribute;
 use Grayloon\MagentoStorage\Support\MagentoProductAttributes;
+use Grayloon\MagentoStorage\Tests\TestCase;
+use Illuminate\Support\Facades\Http;
 
 class MagentoProductAttributesTest extends TestCase
 {
@@ -30,5 +32,136 @@ class MagentoProductAttributesTest extends TestCase
         Http::fake(fn () => Http::response([], 200));
 
         (new MagentoProductAttributes)->count();
+    }
+
+    public function test_creates_magento_product_attribute_from_api()
+    {
+        $attribute = (new MagentoProductAttributes())->updateOrCreate([
+            'attribute_id'    => 300,
+            'frontend_labels' => [
+                [
+                    'store_id' => 1,
+                    'label'    => 'foo',
+                ],
+                [
+                    'store_id' => 2,
+                    'label'    => 'bar',
+                ]
+            ],
+            'default_frontend_label' => 'foo',
+            'attribute_code'         => 'test_attribute',
+            'position'               => 4,
+            'default_value'          => '',
+            'frontend_input'         => 'select',
+        ]);
+
+        $this->assertEquals(1, MagentoProductAttribute::count());
+
+        $this->assertEquals(300, $attribute->id);
+        $this->assertEquals('foo', $attribute->name);
+        $this->assertEquals('test_attribute', $attribute->code);
+        $this->assertEquals(4, $attribute->position);
+        $this->assertEmpty($attribute->default_value);
+        $this->assertEquals('select', $attribute->type);
+    }
+
+    public function test_updates_magento_product_attribute_from_api()
+    {
+        $existingAttribute = MagentoProductAttributeFactory::new()->create([
+            'id' => 300,
+        ]);
+        $attribute = (new MagentoProductAttributes())->updateOrCreate([
+            'attribute_id'    => 300,
+            'frontend_labels' => [
+                [
+                    'store_id' => 1,
+                    'label'    => 'foo',
+                ],
+                [
+                    'store_id' => 2,
+                    'label'    => 'bar',
+                ]
+            ],
+            'default_frontend_label' => 'foo',
+            'attribute_code'         => 'test_attribute',
+            'position'               => 4,
+            'default_value'          => '',
+            'frontend_input'         => 'select',
+        ]);
+
+        $this->assertEquals(1, MagentoProductAttribute::count());
+
+        $this->assertEquals(300, $attribute->id);
+        $this->assertEquals('foo', $attribute->name);
+        $this->assertEquals('test_attribute', $attribute->code);
+        $this->assertEquals(4, $attribute->position);
+        $this->assertEmpty($attribute->default_value);
+        $this->assertEquals('select', $attribute->type);
+    }
+
+    public function test_resolves_attribute_label_without_config_without_available_labels_with_default()
+    {
+        $label = (new MagentoProductAttributes)->resolveAttributeLabel([], 'foo');
+
+        $this->assertEquals('foo', $label);
+    }
+
+    public function test_resolves_attribute_label_with_config_without_available_labels_with_default()
+    {
+        config(['magento.default_store_id' => 1]);
+
+        $label = (new MagentoProductAttributes)->resolveAttributeLabel([], 'foo');
+
+        $this->assertEquals('foo', $label);
+    }
+
+    public function test_resolves_attribute_label_without_config_with_available_labels_without_default()
+    {
+        $label = (new MagentoProductAttributes)->resolveAttributeLabel([
+            [
+                'store_id' => 1,
+                'label'    => 'foo',
+            ],
+            [
+                'store_id' => 2,
+                'label'    => 'bar',
+            ],
+        ], '');
+
+        $this->assertEquals('foo', $label);
+    }
+
+    public function test_resolves_attribute_label_with_config_with_available_labels_with_default()
+    {
+        config(['magento.default_store_id' => 2]);
+
+        $label = (new MagentoProductAttributes)->resolveAttributeLabel([
+            [
+                'store_id' => 1,
+                'label'    => 'foo',
+            ],
+            [
+                'store_id' => 2,
+                'label'    => 'bar',
+            ],
+        ], 'foo');
+
+        $this->assertEquals('bar', $label);
+    }
+
+    public function test_resolves_attribute_label_without_config_with_available_labels_with_default()
+    {
+        $label = (new MagentoProductAttributes)->resolveAttributeLabel([
+            [
+                'store_id' => 1,
+                'label'    => 'foo',
+            ],
+            [
+                'store_id' => 2,
+                'label'    => 'bar',
+            ],
+        ], 'bar');
+
+        $this->assertEquals('bar', $label);
     }
 }
